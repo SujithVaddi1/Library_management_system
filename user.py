06273d515d39535abc14ef37dd187823d950e1e9
SujithVaddi1 <vaddisujith18@gmail.com>
2025-11-13 18:44:38 +0530

    Create user.py

---
+++ user.py
@@ -0,0 +1,18 @@
+from exceptions import BookNotFoundException
+
+class User:
+    def __init__(self, name: str, library):
+        self.name = name
+        self.library = library
+
+    def add_book_to_library(self, book):
+        self.library.add_book(book)
+
+    def remove_book_from_library(self, title):
+        try:
+            self.library.remove_book(title)
+        except BookNotFoundException as e:
+            print(e)
+
+    def view_books(self):
+        self.library.list_books()
