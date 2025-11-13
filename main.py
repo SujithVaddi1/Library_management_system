399bcb9c931789307de5c2eecb1bd954bf209964
SujithVaddi1 <vaddisujith18@gmail.com>
2025-11-13 19:05:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from library import Library
-from user import User
-from book import Book
+from .library import Library
+from .user import User
+from .book import Book
+
 
 def main():
     library = Library()
